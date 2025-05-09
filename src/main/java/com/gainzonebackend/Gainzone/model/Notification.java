3db17dc3630cf762540965574b532b667f00df05
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 21:29:40 +0200

    initial-repo-commit

--- src/main/java/com/gainzonebackend/Gainzone/model/Notification.java
+++ src/main/java/com/gainzonebackend/Gainzone/model/Notification.java
@@ -1,2 +1,34 @@
-package com.gainzonebackend.Gainzone.model;public class Notification {
+package com.gainzonebackend.Gainzone.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@Table(name = "notifications")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type; // EMAIL, SMS, IN_APP=
+    private LocalDateTime sentAt;
+    private Boolean isRead;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
 }
+
