cb94938bd899e5f1dc6995c2b889dd1daa8fe0cf
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 17:55:20 +0200

    fifth-entities-commit

--- src/main/java/com/gainzonebackend/Gainzone/model/Integration.java
+++ src/main/java/com/gainzonebackend/Gainzone/model/Integration.java
@@ -1,10 +1,13 @@
 package com.gainzonebackend.Gainzone.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -16,4 +19,14 @@ public class Integration {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String provider;
+    private String accessToken; // OAuth token for API access
+    private LocalDateTime connectedAt;
+    private LocalDateTime lastSyncedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
 }
