b3ffa6f63632ecf1949635f7af53795763139bbb
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 09:54:53 +0200

    initial-commit

---
+++ src/main/java/com/gainzonebackend/Gainzone/GainzoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gainzonebackend.Gainzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GainzoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GainzoneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gainzonebackend/Gainzone/GainzoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gainzonebackend.Gainzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GainzoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
