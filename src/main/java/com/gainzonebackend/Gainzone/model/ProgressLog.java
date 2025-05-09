f7e4028d3bf0319d0fa6f248180feac2cfd79aec
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 17:51:14 +0200

    fifth-entities-commit

--- src/main/java/com/gainzonebackend/Gainzone/model/ProgressLog.java
+++ src/main/java/com/gainzonebackend/Gainzone/model/ProgressLog.java
@@ -1,2 +1,33 @@
-package com.gainzonebackend.Gainzone.model;public class ProgressLog {
+package com.gainzonebackend.Gainzone.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@Entity
+@Table(name = "progress_logs")
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ProgressLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Double weight;
+    private Double bodyFatPercentage;
+    private Integer caloriesBurned;
+    private LocalDateTime loggedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
 }
--- src/main/java/com/gainzonebackend/Gainzone/model/Subscription.java
+++ src/main/java/com/gainzonebackend/Gainzone/model/Subscription.java
@@ -1,2 +1,20 @@
-package com.gainzonebackend.Gainzone.model;public class Subscription {
+package com.gainzonebackend.Gainzone.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@Table(name = "subscription")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
 }
