3e4a48dc193e69dc9997f7cbf8c266910c14b6a8
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 17:52:28 +0200

    fifth-entities-commit

--- src/main/java/com/gainzonebackend/Gainzone/model/Subscription.java
+++ src/main/java/com/gainzonebackend/Gainzone/model/Subscription.java
@@ -1,10 +1,13 @@
 package com.gainzonebackend.Gainzone.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -16,5 +19,21 @@ public class Subscription {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+    private String stripeSubscriptionId;
+
+    @Enumerated(EnumType.STRING)
+    private SubscriptionPlan plan;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    @Enumerated(EnumType.STRING)
+    private SubscriptionStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
 }
