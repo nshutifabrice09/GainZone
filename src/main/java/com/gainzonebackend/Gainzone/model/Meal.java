5fa98605288955738658bf59fefeebeb02ab74fc
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 17:50:08 +0200

    fifth-entities-commit

--- src/main/java/com/gainzonebackend/Gainzone/model/Meal.java
+++ src/main/java/com/gainzonebackend/Gainzone/model/Meal.java
@@ -1,2 +1,34 @@
-package com.gainzonebackend.Gainzone.model;public class Meal {
+package com.gainzonebackend.Gainzone.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@Entity
+@Table(name = "meals")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Meal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String ingredients;
+    private Integer calories;
+    private LocalDateTime scheduledDate;
+    private Boolean logged;
+
+    @ManyToOne
+    @JoinColumn(name = "nutrition_plan_id")
+    @JsonIgnore
+    private NutritionPlan nutritionPlan;
+
 }
