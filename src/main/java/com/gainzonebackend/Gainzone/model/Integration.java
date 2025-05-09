6bf674510eb3b9a3568f05a5a887abb8e1a41d55
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 17:53:24 +0200

    fifth-entities-commit

--- src/main/java/com/gainzonebackend/Gainzone/model/Integration.java
+++ src/main/java/com/gainzonebackend/Gainzone/model/Integration.java
@@ -1,2 +1,19 @@
-package com.gainzonebackend.Gainzone.model;public class Integration {
+package com.gainzonebackend.Gainzone.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@Entity
+@Table(name = "integrations")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Integration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
