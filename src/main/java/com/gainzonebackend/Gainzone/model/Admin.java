d367a7cb1c3fc2e7a82e98f835dc15ab506d09ef
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 11:46:09 +0200

    second-entities-commit

--- src/main/java/com/gainzonebackend/Gainzone/model/Admin.java
+++ src/main/java/com/gainzonebackend/Gainzone/model/Admin.java
@@ -1,5 +1,21 @@
 package com.gainzonebackend.Gainzone.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
 }
