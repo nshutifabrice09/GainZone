3aebdedac5fdf0698ce417bdc1e2be59762d25b9
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-09 21:40:05 +0200

    final-repo-commit

--- src/main/java/com/gainzonebackend/Gainzone/repository/UserRepository.java
+++ src/main/java/com/gainzonebackend/Gainzone/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.gainzonebackend.Gainzone.repository;
 
 import com.gainzonebackend.Gainzone.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository <User, Long> {
 }
--- src/main/java/com/gainzonebackend/Gainzone/repository/WorkoutPlanRepository.java
+++ src/main/java/com/gainzonebackend/Gainzone/repository/WorkoutPlanRepository.java
@@ -2,6 +2,8 @@ package com.gainzonebackend.Gainzone.repository;
 
 import com.gainzonebackend.Gainzone.model.WorkoutPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WorkoutPlanRepository extends JpaRepository<WorkoutPlan, Long> {
 }
