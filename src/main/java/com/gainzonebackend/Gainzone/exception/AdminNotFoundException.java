7836d1cf5d5bf2908fe19ef893bfd3c38437d4bc
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 20:48:50 +0200

    service-commit

--- src/main/java/com/gainzonebackend/Gainzone/exception/AdminNotFoundException.java
+++ src/main/java/com/gainzonebackend/Gainzone/exception/AdminNotFoundException.java
@@ -1,2 +1,8 @@
-package com.gainzonebackend.Gainzone.exception;public class AdminNotFoundException {
+package com.gainzonebackend.Gainzone.exception;
+
+public class AdminNotFoundException extends RuntimeException{
+    public AdminNotFoundException(String username){
+        super
+                ("Couldn't find an Admin with username "+username);
+    }
 }
--- src/main/java/com/gainzonebackend/Gainzone/service/AdminService.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/AdminService.java
@@ -1,2 +1,15 @@
-package com.gainzonebackend.Gainzone.service;public interface AdminService {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.Admin;
+
+import java.util.List;
+
+public interface AdminService {
+    List<Admin> getAllAdmins();
+    Admin getAdminByUsername(String username);
+    Admin saveAdmin(Admin admin);
+
+    Admin updateAdmin(String username, Admin admin);
+
+    void removeByUsername(String username);
 }
