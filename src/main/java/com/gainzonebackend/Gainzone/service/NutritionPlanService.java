6d1b325e027942f1adf02d455763f109c270f98d
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 21:15:32 +0200

    service-commit

--- src/main/java/com/gainzonebackend/Gainzone/service/NutritionPlanService.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/NutritionPlanService.java
@@ -1,2 +1,13 @@
-package com.gainzonebackend.Gainzone.service;public interface NutritionPlanServive {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.NutritionPlan;
+
+import java.util.List;
+
+public interface NutritionPlanService {
+    List<NutritionPlan> getAllNutritionPlans();
+    NutritionPlan getNutritionPlanById(Long id);
+    NutritionPlan saveNutritionPlan(NutritionPlan nutritionPlan);
+    NutritionPlan updateNutritionPlan(Long id, NutritionPlan nutritionPlan);
+    void removeById(Long id);
 }
--- src/main/java/com/gainzonebackend/Gainzone/service/ProgressLogService.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/ProgressLogService.java
@@ -1,2 +1,13 @@
-package com.gainzonebackend.Gainzone.service;public interface ProgressLogService {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.ProgressLog;
+
+import java.util.List;
+
+public interface ProgressLogService {
+    List<ProgressLog> getAllProgressLogs();
+    ProgressLog getProgressLogById(Long id);
+    ProgressLog saveProgressLog(ProgressLog progressLog);
+    ProgressLog updateProgressLog(Long id, ProgressLog progressLog);
+    void removeById(Long id);
 }
