6f5898d2b20aab9191c1929fff770807038a28c0
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 21:12:43 +0200

    service-commit

--- src/main/java/com/gainzonebackend/Gainzone/service/NotificationService.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/NotificationService.java
@@ -1,2 +1,13 @@
-package com.gainzonebackend.Gainzone.service;public interface NotificationService {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.Notification;
+
+import java.util.List;
+
+public interface NotificationService {
+    List<Notification> getAAllNotifications();
+    Notification getNotificationById(Long id);
+    Notification saveNotification(Notification notification);
+    Notification updateNotification(Long id, Notification notification);
+    void removeById(Long id);
 }
