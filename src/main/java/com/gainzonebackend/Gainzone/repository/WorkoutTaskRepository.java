e7dbbbb94894ccf1b224029e94ce83135644c58c
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 13:53:42 +0200

    initial-service-commit

--- src/main/java/com/gainzonebackend/Gainzone/repository/WorkoutTaskRepository.java
+++ src/main/java/com/gainzonebackend/Gainzone/repository/WorkoutTaskRepository.java
@@ -1,2 +1,9 @@
-package com.gainzonebackend.Gainzone.repository;public class WorkoutTask {
+package com.gainzonebackend.Gainzone.repository;
+
+import com.gainzonebackend.Gainzone.model.WorkoutTask;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkoutTaskRepository extends JpaRepository <WorkoutTask, Long>{
 }
