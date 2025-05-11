f5d5ff81a37192c245039fe1eea36cccf5ac28a3
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 21:19:09 +0200

    initial-serviceImpl-commit

--- src/main/java/com/gainzonebackend/Gainzone/service/WorkoutTaskService.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/WorkoutTaskService.java
@@ -1,2 +1,13 @@
-package com.gainzonebackend.Gainzone.service;public interface WorkoutTaskService {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.WorkoutTask;
+
+import java.util.List;
+
+public interface WorkoutTaskService {
+    List<WorkoutTask> getAllWorkoutTasks();
+    WorkoutTask getWorkoutTaskById(Long id);
+    WorkoutTask saveWorkoutTask(WorkoutTask workoutTask);
+    WorkoutTask updateWorkoutTask(Long id, WorkoutTask workoutTask);
+    void removeById(Long id);
 }
