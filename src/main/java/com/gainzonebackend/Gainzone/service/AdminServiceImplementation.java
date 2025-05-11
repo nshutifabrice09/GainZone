8bf630359bd0c5c8d22eef29eeb53ee9942fa6a0
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 21:22:16 +0200

    serviceImpl-commit

--- src/main/java/com/gainzonebackend/Gainzone/service/AdminServiceImplementation.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/AdminServiceImplementation.java
@@ -1,2 +1,35 @@
-package com.gainzonebackend.Gainzone.service;public class AdminServiceImplementation {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.Admin;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdminServiceImplementation implements AdminService{
+
+    @Override
+    public List<Admin> getAllAdmins() {
+        return null;
+    }
+
+    @Override
+    public Admin getAdminByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Admin saveAdmin(Admin admin) {
+        return null;
+    }
+
+    @Override
+    public Admin updateAdmin(String username, Admin admin) {
+        return null;
+    }
+
+    @Override
+    public void removeByUsername(String username) {
+
+    }
 }
--- src/main/java/com/gainzonebackend/Gainzone/service/IntegrationServiceImplementation.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/IntegrationServiceImplementation.java
@@ -1,2 +1,34 @@
-package com.gainzonebackend.Gainzone.service;public class IntegrationServiceImplementation {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.Integration;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IntegrationServiceImplementation implements IntegrationService{
+    @Override
+    public List<Integration> getAllIntegrations() {
+        return null;
+    }
+
+    @Override
+    public Integration getIntegrationById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Integration saveIntegration(Integration integration) {
+        return null;
+    }
+
+    @Override
+    public Integration updateIntegration(Long id, Integration integration) {
+        return null;
+    }
+
+    @Override
+    public void removeById(Long id) {
+
+    }
 }
