be7ddfdfa7b0ecdbcd4661a145795be743d96038
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 21:08:39 +0200

    service-commit

--- src/main/java/com/gainzonebackend/Gainzone/service/IntegrationService.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/IntegrationService.java
@@ -1,2 +1,13 @@
-package com.gainzonebackend.Gainzone.service;public interface IntegrationService {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.Integration;
+
+import java.util.List;
+
+public interface IntegrationService {
+    List<Integration> getAllIntegrations();
+    Integration getIntegrationById(Long id);
+    Integration saveIntegration(Integration integration);
+    Integration updateIntegration(Long id, Integration integration);
+    void removeById(Long id);
 }
--- src/main/java/com/gainzonebackend/Gainzone/service/MealService.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/MealService.java
@@ -1,2 +1,13 @@
-package com.gainzonebackend.Gainzone.service;public class MealService {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.Meal;
+
+import java.util.List;
+
+public interface MealService {
+    List<Meal> getAllMeals();
+    Meal getMealById(Long id);
+    Meal saveMeal(Meal meal);
+    Meal updateMeal(Long id, Meal meal);
+    void removeById(Long id);
 }
