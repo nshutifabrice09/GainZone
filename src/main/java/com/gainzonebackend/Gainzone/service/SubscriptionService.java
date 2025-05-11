240204bf3bf5ee1dd5a858d0250ad0458ae2d672
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-11 21:17:52 +0200

    final-service-commit

--- src/main/java/com/gainzonebackend/Gainzone/service/SubscriptionService.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/SubscriptionService.java
@@ -1,2 +1,13 @@
-package com.gainzonebackend.Gainzone.service;public interface SubscriptionService {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.Subscription;
+
+import java.util.List;
+
+public interface SubscriptionService {
+    List<Subscription> getAllSubscriptions();
+    Subscription getSubscriptionById(Long id);
+    Subscription saveSubscription(Subscription subscription);
+    Subscription updateSubscription(Long id, Subscription subscription);
+    void removeById(Long id);
 }
--- src/main/java/com/gainzonebackend/Gainzone/service/WorkoutPlanService.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/WorkoutPlanService.java
@@ -1,2 +1,13 @@
-package com.gainzonebackend.Gainzone.service;public interface WorkoutPlanService {
+package com.gainzonebackend.Gainzone.service;
+
+import com.gainzonebackend.Gainzone.model.WorkoutPlan;
+
+import java.util.List;
+
+public interface WorkoutPlanService {
+    List<WorkoutPlan> getAllWorkoutPlans();
+    WorkoutPlan getWorkoutPlanById(Long id);
+    WorkoutPlan saveWorkoutPlan(WorkoutPlan workoutPlan);
+    WorkoutPlan updateWorkoutPlan(Long id, WorkoutPlan workoutPlan);
+    void removeById(Long id);
 }
