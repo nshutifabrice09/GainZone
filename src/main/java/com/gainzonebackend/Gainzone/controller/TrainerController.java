ec837a99107d3375a05b88f137eb96f137327090
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-12 09:55:36 +0200

    controller-commit

--- src/main/java/com/gainzonebackend/Gainzone/controller/TrainerController.java
+++ src/main/java/com/gainzonebackend/Gainzone/controller/TrainerController.java
@@ -1,2 +1,49 @@
-package com.gainzonebackend.Gainzone.controller;public class TrainerController {
+package com.gainzonebackend.Gainzone.controller;
+
+import com.gainzonebackend.Gainzone.model.Trainer;
+import com.gainzonebackend.Gainzone.service.TrainerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class TrainerController {
+    private final TrainerService trainerService;
+
+    @Autowired
+    public TrainerController(TrainerService trainerService){
+        this.trainerService = trainerService;
+    }
+
+    @PostMapping("/trainer")
+    public Trainer save(@RequestBody Trainer trainer){
+        return trainerService.saveTrainer(trainer);
+    }
+
+    @GetMapping("/trainers")
+    public List<Trainer> trainerList(){
+        return trainerService.getAllTrainers();
+    }
+
+    @GetMapping("/trainers/{id}")
+    public Trainer getTrainerById(@PathVariable("id") Long id){
+        return trainerService.getTrainerById(id);
+    }
+
+    @GetMapping("/trainer/{cid}")
+    public Trainer findTrainerByCid(@PathVariable("cid") String cid){
+        return trainerService.getTrainerByCid(cid);
+    }
+
+    @DeleteMapping("/trainer/{cid}")
+    public void removeByCid(@PathVariable("cid") String cid){
+        trainerService.removeByCid(cid);
+    }
+
+    @PutMapping("/trainer/{cid}")
+    public Trainer updateTrainer(@PathVariable("cid") String cid, @RequestBody Trainer trainer){
+        return trainerService.updateTrainer(cid, trainer);
+    }
 }
