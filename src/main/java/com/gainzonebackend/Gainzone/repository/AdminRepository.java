560e4b49f9f7f00e8253ef728e9bcf4e6e57e08e
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-12 13:06:42 +0200

    major-update

--- src/main/java/com/gainzonebackend/Gainzone/repository/AdminRepository.java
+++ src/main/java/com/gainzonebackend/Gainzone/repository/AdminRepository.java
@@ -1,9 +1,21 @@
 package com.gainzonebackend.Gainzone.repository;
 
 import com.gainzonebackend.Gainzone.model.Admin;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminRepository extends JpaRepository <Admin, Long> {
+
+    Admin findByUsername(String username);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Admin a WHERE a.username = :username")
+    void removeByUsername(@Param("username") String username);
+
 }
