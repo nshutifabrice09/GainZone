cd0c4a196aed4a54edba97a36e4b278a9ef05e67
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-12 21:16:32 +0200

    major-model-update

--- src/main/java/com/gainzonebackend/Gainzone/model/Notification.java
+++ src/main/java/com/gainzonebackend/Gainzone/model/Notification.java
@@ -30,5 +30,53 @@ public class Notification {
     @JoinColumn(name = "user_id")
     @JsonIgnore
     private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public NotificationType getType() {
+        return type;
+    }
+
+    public void setType(NotificationType type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getSentAt() {
+        return sentAt;
+    }
+
+    public void setSentAt(LocalDateTime sentAt) {
+        this.sentAt = sentAt;
+    }
+
+    public Boolean getRead() {
+        return isRead;
+    }
+
+    public void setRead(Boolean read) {
+        isRead = read;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
 
