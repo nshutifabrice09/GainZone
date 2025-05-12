1daa516e171bf8defc93cae9eb49032f523fe087
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-12 09:52:25 +0200

    controller-commit

--- src/main/java/com/gainzonebackend/Gainzone/controller/AdminController.java
+++ src/main/java/com/gainzonebackend/Gainzone/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.gainzonebackend.Gainzone.controller;
 
+import com.gainzonebackend.Gainzone.model.Admin;
+import com.gainzonebackend.Gainzone.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
