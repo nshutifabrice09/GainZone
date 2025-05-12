7a75a6c1b0e66cd8f7ed7339c1cf13afa29e6d7c
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-12 11:36:14 +0200

    repo-update-commit

--- src/main/java/com/gainzonebackend/Gainzone/controller/SubscriptionController.java
+++ src/main/java/com/gainzonebackend/Gainzone/controller/SubscriptionController.java
@@ -1,2 +1,40 @@
-package com.gainzonebackend.Gainzone.controller;public class SubscriptionController {
+package com.gainzonebackend.Gainzone.controller;
+
+import com.gainzonebackend.Gainzone.model.Subscription;
+import com.gainzonebackend.Gainzone.service.SubscriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class SubscriptionController {
+    private final SubscriptionService subscriptionService;
+
+    @Autowired
+    public SubscriptionController(SubscriptionService subscriptionService) {
+        this.subscriptionService = subscriptionService;
+    }
+
+    @PostMapping("/subscription/{userId}")
+    public Subscription saveSubscription(@RequestBody Subscription subscription) {
+        return subscriptionService.saveSubscription(subscription);
+    }
+
+    @GetMapping("/subscriptions")
+    public List<Subscription> subscriptionList() {
+        return subscriptionService.getAllSubscriptions();
+    }
+
+    @GetMapping("/subscription/{id}")
+    public Subscription getSubscriptionById(@PathVariable("id") Long id) {
+        return subscriptionService.getSubscriptionById(id);
+    }
+
+    @DeleteMapping("/subscription/{id}")
+    public void removeById(@PathVariable("id") Long id) {
+        subscriptionService.removeById(id);
+    }
+
 }
--- src/main/java/com/gainzonebackend/Gainzone/repository/UserRepository.java
+++ src/main/java/com/gainzonebackend/Gainzone/repository/UserRepository.java
@@ -1,9 +1,20 @@
 package com.gainzonebackend.Gainzone.repository;
 
 import com.gainzonebackend.Gainzone.model.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository <User, Long> {
+
+    User findByMembershipId(String membershipId);
+    User findUserById(Long id);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.membershipId = :membershipId")
+    void removeByMembershipId(@Param("membershipId") String membershipId);
 }
