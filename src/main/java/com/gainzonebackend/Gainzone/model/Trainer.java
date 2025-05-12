8d2a2a66504eb410f63600e5a10f5c9b60f5217d
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-12 21:21:43 +0200

    major-model

--- src/main/java/com/gainzonebackend/Gainzone/model/Trainer.java
+++ src/main/java/com/gainzonebackend/Gainzone/model/Trainer.java
@@ -37,4 +37,108 @@ public class Trainer {
 
     @OneToMany
     private List<NutritionPlan> nutritionPlans;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
+
+    public String getCertifications() {
+        return certifications;
+    }
+
+    public void setCertifications(String certifications) {
+        this.certifications = certifications;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public List<WorkoutPlan> getWorkoutPlan() {
+        return workoutPlan;
+    }
+
+    public void setWorkoutPlan(List<WorkoutPlan> workoutPlan) {
+        this.workoutPlan = workoutPlan;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<NutritionPlan> getNutritionPlans() {
+        return nutritionPlans;
+    }
+
+    public void setNutritionPlans(List<NutritionPlan> nutritionPlans) {
+        this.nutritionPlans = nutritionPlans;
+    }
 }
