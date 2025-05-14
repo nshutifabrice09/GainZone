3aff3f30460119dc7d2580f146643de06dceddcd
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-14 14:31:46 +0200

    service-impl-updates

--- src/main/java/com/gainzonebackend/Gainzone/service/TrainerServiceImplementation.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/TrainerServiceImplementation.java
@@ -1,12 +1,21 @@
 package com.gainzonebackend.Gainzone.service;
 
 import com.gainzonebackend.Gainzone.model.Trainer;
+import com.gainzonebackend.Gainzone.repository.TrainerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TrainerServiceImplementation implements TrainerService{
+
+    private TrainerRepository trainerRepository;
+
+    @Autowired
+    public TrainerServiceImplementation(TrainerRepository trainerRepository){
+        this.trainerRepository = trainerRepository;
+    }
     @Override
     public List<Trainer> getAllTrainers() {
         return null;
