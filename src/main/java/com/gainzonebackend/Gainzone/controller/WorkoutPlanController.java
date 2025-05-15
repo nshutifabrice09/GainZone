739dc136fc60be0120224ef09c24156ff91958aa
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-15 14:39:57 +0200

    second-controller-updates

--- src/main/java/com/gainzonebackend/Gainzone/controller/WorkoutPlanController.java
+++ src/main/java/com/gainzonebackend/Gainzone/controller/WorkoutPlanController.java
@@ -1,4 +1,19 @@
 package com.gainzonebackend.Gainzone.controller;
 
+
+import com.gainzonebackend.Gainzone.service.WorkoutPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
 public class WorkoutPlanController {
+
+    private final WorkoutPlanService workoutPlanService;
+
+    @Autowired
+    public WorkoutPlanController(WorkoutPlanService workoutPlanService) {
+        this.workoutPlanService = workoutPlanService;
+    }
 }
