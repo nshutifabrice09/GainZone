5b4269a5f068a06adf42e51deef3b53f0eaf6783
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-15 23:07:30 +0200

    service-implementation-updates

--- src/main/java/com/gainzonebackend/Gainzone/controller/WorkoutTaskController.java
+++ src/main/java/com/gainzonebackend/Gainzone/controller/WorkoutTaskController.java
@@ -1,2 +1,23 @@
-package com.gainzonebackend.Gainzone.controller;public class WorkoutTaskController {
+package com.gainzonebackend.Gainzone.controller;
+
+import com.gainzonebackend.Gainzone.model.WorkoutTask;
+import com.gainzonebackend.Gainzone.service.WorkoutTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class WorkoutTaskController {
+
+    private final WorkoutTaskService workoutTaskService;
+
+    @Autowired
+    public WorkoutTaskController(WorkoutTaskService workoutTaskService) {
+        this.workoutTaskService = workoutTaskService;
+    }
+
+    @PostMapping("/workoutTask/{workoutPlanId}")
+    public WorkoutTask save(@RequestBody WorkoutTask workoutTask, @PathVariable ("workoutPlanId") Long workoutPlanId){
+        return workoutTaskService.saveWorkoutTask(workoutTask, workoutPlanId);
+    }
 }
