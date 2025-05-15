6cb0490981d47746d316f35b8b4612e9ca3bbec1
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-15 22:02:38 +0200

    controller-updates

--- src/main/java/com/gainzonebackend/Gainzone/service/WorkoutTaskServiceImplementation.java
+++ src/main/java/com/gainzonebackend/Gainzone/service/WorkoutTaskServiceImplementation.java
@@ -1,12 +1,25 @@
 package com.gainzonebackend.Gainzone.service;
 
 import com.gainzonebackend.Gainzone.model.WorkoutPlan;
+import com.gainzonebackend.Gainzone.repository.WorkoutPlanRepository;
+import com.gainzonebackend.Gainzone.repository.WorkoutTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class WorkoutTaskServiceImplementation implements WorkoutPlanService{
+
+    private final WorkoutTaskRepository workoutTaskRepository;
+    private final WorkoutPlanRepository workoutPlanRepository;
+
+    @Autowired
+    public WorkoutTaskServiceImplementation(WorkoutTaskRepository workoutTaskRepository, WorkoutPlanRepository workoutPlanRepository) {
+        this.workoutTaskRepository = workoutTaskRepository;
+        this.workoutPlanRepository = workoutPlanRepository;
+    }
+
     @Override
     public List<WorkoutPlan> getAllWorkoutPlans() {
         return null;
