640605fcc74f8d652ba3bf5f2def51a2faa630f4
nshutifabrice <nshuti.fabrice09@gmail.com>
2025-05-20 10:11:09 +0200

    initial-exception-commit

--- src/main/java/com/gainzonebackend/Gainzone/exception/SubscriptionNotFoundException.java
+++ src/main/java/com/gainzonebackend/Gainzone/exception/SubscriptionNotFoundException.java
@@ -1,2 +1,8 @@
-package com.gainzonebackend.Gainzone.exception;public class SubscriptionNotFoundException {
+package com.gainzonebackend.Gainzone.exception;
+
+public class SubscriptionNotFoundException extends RuntimeException{
+    public SubscriptionNotFoundException(Long id){
+        super
+                ("Couldn't find a Subscription with id "+id);
+    }
 }
--- src/main/java/com/gainzonebackend/Gainzone/exception/TrainerNotFoundException.java
+++ src/main/java/com/gainzonebackend/Gainzone/exception/TrainerNotFoundException.java
@@ -1,2 +1,8 @@
-package com.gainzonebackend.Gainzone.exception;public class TrainerNotFoundException {
+package com.gainzonebackend.Gainzone.exception;
+
+public class TrainerNotFoundException extends RuntimeException{
+    public TrainerNotFoundException(Long id){
+        super
+                ("Couldn't find a Trainer with id "+id);
+    }
 }
